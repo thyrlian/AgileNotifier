149675d625b5cf4bbd1db7ab70e7f507941f473e
Jing Li <thyrlian@gmail.com>
2013-06-16 12:30:18 +0200

    get_committer_name_of_a_commit is working now

--- lib/agile_notifier/github.rb
+++ lib/agile_notifier/github.rb
@@ -1,11 +1,23 @@
 require_relative 'scm'
+require_relative 'response_helper'
+require 'json'
+require 'httparty'
 
 module AgileNotifier
   class Github < SCM
+    extend ResponseHelper
 
-    class Commit < SCM::Commit
-      def author
+    USERAGENT = 'AgileNotifier'
 
+    def self.get_value(key, url)
+      get_value_of_key(key, url, :headers => {'User-Agent' => USERAGENT})
+    end
+
+    class Repository < SCM::Repository
+      def get_committer_name_of_a_commit(revision)
+        url = @url + "/repos/#{@user}/#{@repo}/git/commits/#{revision}"
+        committer = Github.get_value('committer', url)
+        committer['name']
       end
     end
   end
--- lib/agile_notifier/scm.rb
+++ lib/agile_notifier/scm.rb
@@ -1,7 +1,32 @@
 module AgileNotifier
   class SCM
+    attr_accessor :url, :repositories
+
     def initialize(url)
       @url = url
+      @repositories = []
+    end
+
+    def add_repository(repository)
+      @repositories.push(repository)
+    end
+
+    class Repository
+      attr_accessor :user, :repo, :url
+
+      def initialize(args)
+        @user = args[:user]
+        @repo = args[:repo]
+        @url = args[:url]
+      end
+
+      def get_commit(revision)
+        raise(NotImplementedError, "Abstract method [#{__method__}] is called, please implement", caller)
+      end
+
+      def get_committer_of_a_commit(revision)
+        raise(NotImplementedError, "Abstract method [#{__method__}] is called, please implement", caller)
+      end
     end
   end
 end
