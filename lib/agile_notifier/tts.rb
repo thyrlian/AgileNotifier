faf3b69e92ce6708c77fa22373c0057e553763b5
Jing Li <thyrlian@gmail.com>
2013-06-22 16:38:20 +0200

    Completed TTS class, moved download_file, remove_file and play class methods into TTS::Service

--- lib/agile_notifier/tts.rb
+++ lib/agile_notifier/tts.rb
@@ -1,10 +1,9 @@
 require 'uri'
 require 'open-uri'
+require_relative 'player'
 
 module AgileNotifier
   class TTS
-    @@temp_file = File.dirname(__FILE__) + '/temp_tts'
-
     class << self
       def speak(text)
         speak_via('osx_speech', text)
@@ -29,30 +28,38 @@ module AgileNotifier
       def encode_www_form_and_remove_punctuation(text)
         encode_www_form(replace_punctuation_by_space(text))
       end
-
-      def download_file(url, file = @@temp_file)
-        open(url) do |remote_file|
-          data = remote_file.read
-          File.open(file, 'wb') do |local_file|
-            local_file.write(data)
-          end
-        end
-      end
-
-      def remove_file(file = @@temp_file)
-        File.delete(file)
-      end
     end
 
     class Service
+      @@temp_file = File.dirname(__FILE__) + '/temp_tts'
+
       class << self
+        def download_file(url, file = @@temp_file)
+          open(url) do |remote_file|
+            data = remote_file.read
+            File.open(file, 'wb') do |local_file|
+              local_file.write(data)
+            end
+          end
+        end
+
+        def remove_file(file = @@temp_file)
+          File.delete(file)
+        end
+
+        def play(url)
+          download_file(url)
+          Player.play_on_osx(@@temp_file)
+          remove_file
+        end
+
         def osx_speech(text)
           fork { exec "say #{text}"}
         end
 
         def tts_api(text)
           url = 'http://tts-api.com/tts.mp3?q='
-          url + encode_www_form_and_remove_punctuation(text)
+          play(url + encode_www_form_and_remove_punctuation(text))
         end
 
         def mary_tts(text)
@@ -60,11 +67,13 @@ module AgileNotifier
           text = encode_www_form_and_remove_punctuation(text)
           locale = 'de'
           audio_format = 'WAVE_FILE'
-          url + "process?INPUT_TEXT=#{text}&INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&LOCALE=#{locale}&AUDIO=#{audio_format}"
+          play(url + "process?INPUT_TEXT=#{text}&INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&LOCALE=#{locale}&AUDIO=#{audio_format}")
         end
       end
+
+      private_class_method :download_file, :remove_file, :play
     end
 
-    private_class_method :new, :call_service, :replace_punctuation_by_space, :encode_www_form, :encode_www_form_and_remove_punctuation, :download_file, :remove_file
+    private_class_method :new, :call_service, :replace_punctuation_by_space, :encode_www_form, :encode_www_form_and_remove_punctuation
   end
 end
