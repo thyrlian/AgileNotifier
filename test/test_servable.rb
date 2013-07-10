91635ec08bc7d0b826d0768d3a54b17d6fa26fb1
Jing Li <thyrlian@gmail.com>
2013-07-10 07:54:26 +0200

    Added test for Servable

---
+++ test/test_servable.rb
@@ -0,0 +1,16 @@
+require_relative 'helper'
+
+class TestServable < Test::Unit::TestCase
+  def setup
+    @obj = Object.new
+    @obj.extend(Servable)
+  end
+  
+  def test_is_https_request
+    assert_equal(true, @obj.is_https_request?('https://example.com/'))
+  end
+  
+  def test_is_not_https_request
+    assert_equal(false, @obj.is_https_request?('http://example.com/'))
+  end
+end
