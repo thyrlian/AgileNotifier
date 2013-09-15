6473f9ad893a4b9c4c54264b68d2c03c0319c268
Jing Li <thyrlian@gmail.com>
2013-09-15 21:22:12 +0200

    Added language option for Composer

--- lib/agile_notifier/composer.rb
+++ lib/agile_notifier/composer.rb
@@ -2,18 +2,35 @@
 
 module AgileNotifier
   class Composer
-    SENTENCES = [
-        "%{committer_name} hat den Build kaputt gemacht.",
-        "Schießt %{committer_name} mit der Nerf Gun ab!",
-        "%{committer_name} hat Scheiße gebaut."
-    ]
+    SENTENCES = {
+        de: [
+                "%{committer_name} hat den Build kaputt gemacht.",
+                "Schießt %{committer_name} mit der Nerf Gun ab!",
+                "%{committer_name} hat Scheiße gebaut."
+            ],
+        en: [
+                "%{committer_name} has broken the build.",
+                "%{committer_name} fucked up the build.",
+                "What the fucking code has %{committer_name} pushed!"
+            ],
+        zh: [
+                "%{committer_name}在搞毛啊, 构建失败了!",
+                "%{committer_name}提交的什么烂代码啊?",
+                "请注意, %{committer_name}在搞破坏."
+            ]
+    }
 
     class << self
       def blame_committer_of_a_commit(args)
         repo = args[:repo]
         revision = args[:revision] || args[:build].revision
+        language = args[:language]
         committer_name = repo.get_committer_name_of_a_commit(revision)
-        random_picker(SENTENCES).gsub(/%\{committer_name\}/, committer_name)
+        blame_committer(committer_name, language)
+      end
+
+      def blame_committer(committer_name, language)
+        random_picker(SENTENCES[language.to_s.downcase.intern]).gsub(/%\{committer_name\}/, committer_name)
       end
 
       def random_picker(list)
