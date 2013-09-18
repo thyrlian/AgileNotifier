45ee0238eba88f70ba48b8345145cb5f2a95fafa
Jing Li <thyrlian@gmail.com>
2013-09-18 18:01:23 +0200

    Added class method what() to return current os type

--- lib/agile_notifier/operating_system.rb
+++ lib/agile_notifier/operating_system.rb
@@ -1,12 +1,19 @@
 module AgileNotifier
   class OperatingSystem
-    class << self
-      def is_mac?
-        match_os(/darwin/)
-      end
+    TYPE = {
+        lin: 'linux',
+        mac: 'osx',
+        win: 'windows',
+        unknown: 'UNKNOWN'
+    }
 
+    class << self
       def is_linux?
         match_os(/linux/)
+      end
+
+      def is_mac?
+        match_os(/darwin/)
       end
 
       def is_windows?
@@ -15,6 +22,13 @@ module AgileNotifier
 
       def match_os(regex)
         !!RUBY_PLATFORM.match(regex)
+      end
+
+      def what
+        return TYPE[:lin] if is_linux?
+        return TYPE[:mac] if is_mac?
+        return TYPE[:win] if is_windows?
+        return TYPE[:unknown]
       end
     end
 
